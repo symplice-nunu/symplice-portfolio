6594bb79508c742f12f4ce60eda9b46931d51051
symplice-nunu <intwarisymplice@gmail.com>
2023-05-18 22:16:06 +0530

    feat: create languages section

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Introduce from './Sections/Introduce'
 import Skills from './Sections/Skills'
 import Projects from './Sections/Projects'
+import Languages from './Sections/Languages'
 
 function Home() {
   return (
@@ -9,6 +10,7 @@ function Home() {
       <div><Introduce /></div>
       <div> <Skills /> </div>
       <div><Projects /></div>
+      <div><Languages /></div>
     </div>
     
   )
---
+++ src/Components/Sections/Languages.jsx
@@ -0,0 +1,18 @@
+
+import React from 'react'
+
+function Languages() {
+  return (
+    <div>
+        <div className='text-white border my-8 w-[150px] border-gray-600 py-2 px-7 rounded-3xl'>LANGUAGES</div>
+        <div>
+            <div className='text-white text-[25px] mt-2'>English</div>
+            <div className='flex justify-between'>
+            <div className='text-gray-400'>Professional Working Proficiency</div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Languages
