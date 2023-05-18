59dafa3233a877dbb5283811647c2b7c6e9f5982
symplice-nunu <intwarisymplice@gmail.com>
2023-05-18 21:33:47 +0530

    feat: creating worked Projects  data in array

---
+++ src/Components/Data/ProjectsData.jsx
@@ -0,0 +1,68 @@
+export const ProjectsData = [
+    {
+        projectName: 'Hercules freight',
+        projectGit: '',
+        projectLive: 'http://herculesfreight.com',
+        projectImage: '',
+    },
+    {
+        projectName: 'Jc Valves',
+        projectGit: '',
+        projectLive: '',
+        projectImage: '',
+    },
+    {
+        projectName: 'E-KINAMBA',
+        projectGit: 'https://github.com/symplice-nunu/E-KINAMBA',
+        projectLive: '',
+        projectImage: '',
+    },
+    {
+        projectName: 'Real Return On Investiment',
+        projectGit: 'https://github.com/symplice-nunu/Real_Return_On_Investiment',
+        projectLive: '',
+        projectImage: '',
+    },
+    {
+        projectName: 'Smart Monitoring Cricket Club System',
+        projectGit: 'https://github.com/symplice-nunu/Smart_Monitoring_Cricket_Club_System',
+        projectLive: '',
+        projectImage: '',
+    },
+    {
+        projectName: 'Imboni',
+        projectGit: '',
+        projectLive: 'http://imboni.fidalix.com/',
+        projectImage: '',
+    },
+    {
+        projectName: 'Chatpoint',
+        projectGit: 'https://github.com/symplice-nunu/Chatpoint',
+        projectLive: 'https://dev--brilliant-klepon-8f1761.netlify.app/',
+        projectImage: '',
+    },
+    {
+        projectName: 'Online Quiz',
+        projectGit: 'https://github.com/symplice-nunu/Online_Quiz',
+        projectLive: '',
+        projectImage: '',
+    },
+    {
+        projectName: 'Clinic Management System',
+        projectGit: 'https://github.com/symplice-nunu/Clinic_Management_System',
+        projectLive: '',
+        projectImage: '',
+    },
+    {
+        projectName: 'E-Commerce Mobile App',
+        projectGit: '',
+        projectLive: '',
+        projectImage: '',
+    },
+    {
+        projectName: 'Primary School Management',
+        projectGit: 'https://github.com/symplice-nunu/Primary_School',
+        projectLive: '',
+        projectImage: '',
+    }
+]
