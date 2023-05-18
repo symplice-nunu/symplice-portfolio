7199e072acab77fa3511b88624188370bc147369
symplice-nunu <intwarisymplice@gmail.com>
2023-05-18 20:08:50 +0530

    feat: fetching skills data from array

--- src/Components/Data/SkillsData.jsx
+++ src/Components/Data/SkillsData.jsx
@@ -1,97 +1,97 @@
-export const Skills = [
+export const SkillsData = [
     {
         skillsLogo: 'P',
         skillsName: 'PHP',
-        skillsPercentage: '92%',
+        skillsPercentage: '93',
     },
     {
         skillsLogo: 'L',
         skillsName: 'Laravel',
-        skillsPercentage: '91%',
+        skillsPercentage: '91',
     },
     {
         skillsLogo: 'C',
         skillsName: 'CodeIgniter',
-        skillsPercentage: '90%',
+        skillsPercentage: '90',
     },
     {
         skillsLogo: 'P',
         skillsName: 'PowerBi',
-        skillsPercentage: '89%',
+        skillsPercentage: '89',
     },
     {
         skillsLogo: 'F',
         skillsName: 'Figma',
-        skillsPercentage: '80%',
+        skillsPercentage: '80',
     },
     {
         skillsLogo: 'J',
         skillsName: 'JSP',
-        skillsPercentage: '70%',
+        skillsPercentage: '70',
     },
     {
         skillsLogo: 'F',
         skillsName: 'Flutter',
-        skillsPercentage: '70%',
+        skillsPercentage: '70',
     },
     {
         skillsLogo: 'R',
         skillsName: 'React Js',
-        skillsPercentage: '60%',
+        skillsPercentage: '60',
     },
     {
         skillsLogo: 'V',
         skillsName: 'VueJs',
-        skillsPercentage: '55%',
+        skillsPercentage: '55',
     },
     {
         skillsLogo: 'W',
         skillsName: 'Wordpress',
-        skillsPercentage: '55%',
+        skillsPercentage: '55',
     },
     {
         skillsLogo: 'T',
         skillsName: 'TailwindCSS',
-        skillsPercentage: '55%',
+        skillsPercentage: '55',
     },
     {
         skillsLogo: 'H',
         skillsName: 'HTML5',
-        skillsPercentage: '95%',
+        skillsPercentage: '92',
     },
     {
         skillsLogo: 'C',
         skillsName: 'CSS',
-        skillsPercentage: '85%',
+        skillsPercentage: '85',
     },
     {
         skillsLogo: 'M',
         skillsName: 'MySql',
-        skillsPercentage: '87%',
+        skillsPercentage: '87',
     },
     {
         skillsLogo: 'S',
         skillsName: 'Sql Server',
-        skillsPercentage: '75%',
+        skillsPercentage: '75',
     },
     {
         skillsLogo: 'P',
         skillsName: 'Postgress',
-        skillsPercentage: '55%',
+        skillsPercentage: '55',
     },
     {
         skillsLogo: 'F',
         skillsName: 'Firebase',
-        skillsPercentage: '50%',
+        skillsPercentage: '50',
     },
     {
         skillsLogo: 'G',
         skillsName: 'GitHub',
-        skillsPercentage: '85%',
+        skillsPercentage: '85',
     },
     {
         skillsLogo: 'J',
         skillsName: 'Jquery',
-        skillsPercentage: '65%',
+        skillsPercentage: '65',
     }
 ]
--- src/Components/Sections/Skills.jsx
+++ src/Components/Sections/Skills.jsx
@@ -1,15 +1,25 @@
 import React from 'react'
+import { SkillsData } from '../Data/SkillsData'
 
 function Skills() {
   return (
     <div>
         <div className='text-white border my-12 w-[100px] border-gray-600 py-2 px-7 rounded-3xl'>Skills</div>
-        <div>
-            <div className='border-[#8be88d] border w-[160px] px-10 py-6 rounded-t-full rounded-b-full'>
-            <div className=' text-white text-[73px]'>H</div>
-            <div className=' text-[#8be88d] text-[43px]'>92%</div>
-            </div>
-        </div>
+      <div className='flex justify-between flex-wrap'>
+      {
+        SkillsData.map((item, index) => {
+            return (
+                    <div className='grid grid-col'>
+                    <div className={`${item.skillsPercentage >= '90' ? "border-[#8be88d]" : "border-gray-400"} border w-[160px] px-10 py-6 rounded-t-full rounded-b-full`}>
+                    <div className=' text-white text-[73px]'>{item.skillsLogo}</div>
+                    <div className=' text-[#8be88d] text-[43px]'>{item.skillsPercentage}&#37;</div>
+                    </div>
+                    <div className='text-white text-center pt-2 pb-7'>{item.skillsName}</div>
+                </div>
+            )
+        })
+       }
+      </div>
     </div>
   )
 }
