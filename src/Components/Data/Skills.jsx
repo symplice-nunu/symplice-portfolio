ed5a37977ca77b567438cfa87657205191d5d9f9
symplice-nunu <intwarisymplice@gmail.com>
2023-05-18 19:15:56 +0530

    feat: creating my skills data in array

---
+++ src/Components/Data/Skills.jsx
@@ -0,0 +1,97 @@
+export const Skills = [
+    {
+        skillsLogo: 'P',
+        skillsName: 'PHP',
+        skillsPercentage: '92%',
+    },
+    {
+        skillsLogo: 'L',
+        skillsName: 'Laravel',
+        skillsPercentage: '91%',
+    },
+    {
+        skillsLogo: 'C',
+        skillsName: 'CodeIgniter',
+        skillsPercentage: '90%',
+    },
+    {
+        skillsLogo: 'P',
+        skillsName: 'PowerBi',
+        skillsPercentage: '89%',
+    },
+    {
+        skillsLogo: 'F',
+        skillsName: 'Figma',
+        skillsPercentage: '80%',
+    },
+    {
+        skillsLogo: 'J',
+        skillsName: 'JSP',
+        skillsPercentage: '70%',
+    },
+    {
+        skillsLogo: 'F',
+        skillsName: 'Flutter',
+        skillsPercentage: '70%',
+    },
+    {
+        skillsLogo: 'R',
+        skillsName: 'React Js',
+        skillsPercentage: '60%',
+    },
+    {
+        skillsLogo: 'V',
+        skillsName: 'VueJs',
+        skillsPercentage: '55%',
+    },
+    {
+        skillsLogo: 'W',
+        skillsName: 'Wordpress',
+        skillsPercentage: '55%',
+    },
+    {
+        skillsLogo: 'T',
+        skillsName: 'TailwindCSS',
+        skillsPercentage: '55%',
+    },
+    {
+        skillsLogo: 'H',
+        skillsName: 'HTML5',
+        skillsPercentage: '95%',
+    },
+    {
+        skillsLogo: 'C',
+        skillsName: 'CSS',
+        skillsPercentage: '85%',
+    },
+    {
+        skillsLogo: 'M',
+        skillsName: 'MySql',
+        skillsPercentage: '87%',
+    },
+    {
+        skillsLogo: 'S',
+        skillsName: 'Sql Server',
+        skillsPercentage: '75%',
+    },
+    {
+        skillsLogo: 'P',
+        skillsName: 'Postgress',
+        skillsPercentage: '55%',
+    },
+    {
+        skillsLogo: 'F',
+        skillsName: 'Firebase',
+        skillsPercentage: '50%',
+    },
+    {
+        skillsLogo: 'G',
+        skillsName: 'GitHub',
+        skillsPercentage: '85%',
+    },
+    {
+        skillsLogo: 'J',
+        skillsName: 'Jquery',
+        skillsPercentage: '65%',
+    }
+]
