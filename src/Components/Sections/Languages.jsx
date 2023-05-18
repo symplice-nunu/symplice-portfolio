49a33d7b7ec1ae1b9ac43eac1511e9c5f6569691
symplice-nunu <intwarisymplice@gmail.com>
2023-05-18 22:38:30 +0530

    feat: fetching languages data from array

--- src/Components/Sections/Languages.jsx
+++ src/Components/Sections/Languages.jsx
@@ -1,15 +1,24 @@
 
 import React from 'react'
+import { LanguagesData } from '../Data/LanguagesData'
 
 function Languages() {
   return (
     <div>
         <div className='text-white border my-8 w-[150px] border-gray-600 py-2 px-7 rounded-3xl'>LANGUAGES</div>
-        <div>
-            <div className='text-white text-[25px] mt-2'>English</div>
-            <div className='flex justify-between'>
-            <div className='text-gray-400'></div>
-            </div>
+        <div className='space-y-8'>
+            {
+                LanguagesData.map((item,index) => {
+                    return (
+                        <div>
+                            <div className='text-white text-[25px] mt-2'>{item.languageName}</div>
+                            <div className='flex justify-between'>
+                            <div className='text-gray-400'>{item.languageLevel}</div>
+                            </div>
+                        </div>
+                    )
+                })
+            }
         </div>
     </div>
   )
